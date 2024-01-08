638516eea9182e5edf6c62078e4b8d41fb6e8a4b
Abi Jama <abi_jama88@hotmail.com>
2024-01-08 21:46:46 +0000

    trying to fix the connection

--- src/routes/strings.js
+++ src/routes/strings.js
@@ -1,26 +1,26 @@
 const express = require('express');
 
-const stringRouter = express.Router();
+const stringsRouter = express.Router();
 
 const { sayHello, uppercase, lowercase, firstCharacters } = require('../lib/src/strings');
 
-stringRouter.get('/strings/hello/:string', (req, res) => {
+stringsRouter.get('/strings/hello/:string', (req, res) => {
   res.status(200).json({ result: sayHello(req.params.string) });
 });
 
-stringRouter.get('/strings/upper/:string', (req, res) => {
+stringsRouter.get('/strings/upper/:string', (req, res) => {
   res.status(200).json({ result: uppercase(req.params.string) });
 });
 
-stringRouter.get('/strings/lower/:string', (req, res) => {
+stringsRouter.get('/strings/lower/:string', (req, res) => {
   res.status(200).json({ result: lowercase(req.params.string) });
 });
 
-stringRouter.get('/strings/first-characters/:string', (req, res) => {
+stringsRouter.get('/strings/first-characters/:string', (req, res) => {
   const length = req.query.length || 1;
   const inputString = req.params.string;
 
   res.status(200).json({ result: firstCharacters(inputString, length) });
 });
 
-module.exports = stringRouter;
+module.exports = stringsRouter;
