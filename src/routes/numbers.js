2c060c70ebeec3ec00782a02196a376978222fa6
Abi Jama <abi_jama88@hotmail.com>
2024-01-17 20:23:36 +0000

    testing

--- src/routes/numbers.js
+++ src/routes/numbers.js
@@ -23,15 +23,16 @@ numbersRouter.get('/subtract/:a/from/:b', (req, res) => {
 });
 
 numbersRouter.post('/multiply', (req, res) => {
-  if (!req.body.a || !req.body.b) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
+  const { a, b } = req.body;
+
+  if ((a && b) || (a === 0 && b) || (a && b === 0)) {
+    if (Number.isNan(parseInt(a, 10)) || Number.isNan(parseInt(b, 10))) {
+      return res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
+    }
+
+    return res.status(200).send({ result: multiply(a, b) });
   }
-  const a = parseInt(req.body.a, 10);
-  const b = parseInt(req.body.b, 10);
-  if (Number.isNaN(a) || Number.isNaN(b)) {
-    return res.status(400).send({ error: 'Parameters "a" and "b" must be valid numbers.' });
-  }
-  return res.status(200).send({ result: multiply(a, b) });
+  return res.status(400).send({ error: 'Parameters "a" and "b" are required.' });
 });
 
 numbersRouter.post('/divide', (req, res) => {
